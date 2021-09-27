e54077122507c7a010e17e04bf1c33813032fa1b
MARC KAMGA Olivier <kamga_marco@yahoo.com>
2021-09-27 09:30:15 +0200

    Update module2.js

--- module2.js
+++ module2.js
@@ -1,8 +1,10 @@
+//Export all in module1 (file module1.js) to module2
 modules.export('*', 'module1');
 
+//Export the object as 'child' in module2
 modules.export({ name: 'KAMGA', 'firstName' : 'Emmanuel'}, 'child');
 
-
+//Export the function to in module2 with the name 'isTypeOf'
 modules.export(function(o, type) {
     return Object.prototype.toString(o) === '[object ' + type + ']';    
 }, 'isTypeOf');
